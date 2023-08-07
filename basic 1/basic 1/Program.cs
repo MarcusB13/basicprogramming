daf3ca010f36213b9b269d45160fcb8ae72d4833
MarcusB13 <marcusbager@hotmail.com>
2023-08-07 10:25:59 +0200

    første 2 opgaver

--- basic 1/basic 1/Program.cs
+++ basic 1/basic 1/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace basic
+{
 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(add(10, 2, 2));
+            Console.WriteLine(celciustofahrenheit(-20));
+        }
+
+        
+        static int add (int firstNumber, int secondNumber, int lastNumber)
+        {
+            return (firstNumber + secondNumber) * lastNumber;
+        }
+
+        static double celciustofahrenheit (int celcius)
+        {
+            double fahrenheit = (celcius * 9 / 5) + 32;
+
+            if (fahrenheit < -271.15)
+            {
+                fahrenheit = -217.15;
+            }
+            return fahrenheit;
+        }
+    }
+}
